56c8bef937e8d5c1f392c2b097c13ff373d204b0
vliupro <virus_liu@163.com>
2016-06-01 21:08:25 +0800

    添加接口的注释信息

--- src/main/java/me/vliupro/smb/service/WeiboService.java
+++ src/main/java/me/vliupro/smb/service/WeiboService.java
@@ -10,13 +10,39 @@ import java.util.List;
  */
 public interface WeiboService {
 
+    /**
+     * 发表微博
+     * @param weibo
+     * @return
+     */
     boolean publish(Weibo weibo);
 
+    /**
+     * 删除微博
+     * @param weiboId
+     * @return
+     */
     boolean deleteById(int weiboId);
 
+    /**
+     * 根据weiboId获取微博
+     * @param weiboId
+     * @return
+     */
     Weibo getWeiboById(int weiboId);
 
+    /**
+     * 获取最新微博，分页
+     * @param pageNum
+     * @param total
+     * @return
+     */
     Page<Weibo> getWeibosByPage(int pageNum, int total);
 
+    /**
+     *
+     * @param userId
+     * @return
+     */
     List<Weibo> getWeibosByUserId(int userId);
 }
